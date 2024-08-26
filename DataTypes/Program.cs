ef82eb0c82661ae5ca5cbf07d04fc52f23d25cea
amman <amman@DESKTOP-VO0EAQH>
2024-08-26 08:06:29 +0100

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿/ See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApplication
+
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 18;
+            double myDoubleNum = 5.124;
+            bool myBool = true;
+            char myChar = 'a';
+
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myChar);
+        }
+    }
+
+
+
+
+}
---
+++ variables/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace MyApplication
+
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "amman";
+            Console.WriteLine(name);
+        }
+    }
+
+}
+
