48ee64410c25d27f1389d46174582a9f5287a6b4
ammanh <amman@DESKTOP-VO0EAQH>
2024-08-31 00:15:09 +0530

    switch command

---
+++ ifCondition/Program.cs
@@ -0,0 +1,66 @@
+﻿//namespace myApplication
+
+//{
+
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            {
+//                string isRaining = "yes";
+//                string haveUmbrella = "no";
+
+//                if (isRaining == "yes")
+//                {
+//                    Console.WriteLine("It's raining outside");
+
+//                    if (haveUmbrella == "yes")
+//                    {
+//                        Console.WriteLine("You have an umbrella");
+//                    }
+//                    else
+//                    {
+//                        Console.WriteLine("You don't have an umbrella");
+//                    }
+//                }
+//                else
+//                {
+//                    Console.WriteLine("It's not raining outside");
+//                }
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+using System;
+
+namespace myApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is it raining? (yes/no)");
+            string isRaining = Console.ReadLine().ToLower();
+
+            if (isRaining == "yes")
+            {
+                Console.WriteLine("It's raining outside");
+            }
+            else if (isRaining == "no")
+            {
+                Console.WriteLine("It's not raining outside");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Enter 'yes' or 'no'.");
+            }
+        }
+    }
+}
+
---
+++ switch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace myApplication
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number 1-7");
+
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Invalid number entered");
+                    break;
+
+
+
+            }
+        }
+    }
+
+}
