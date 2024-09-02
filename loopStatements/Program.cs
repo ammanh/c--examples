7bec7e7f43d926b0805f03e882ee91a4a02adfe8
ammanh <amman@DESKTOP-VO0EAQH>
2024-09-02 18:13:12 +0530

    loops

---
+++ loopStatements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace myApplication
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string playAgain;
+
+            do
+            {
+                int targetNumber = 57;
+                int guess;
+
+                Console.WriteLine("Guess the number i am thinkuing of between 1 and 100!");
+
+                do
+                {
+                    Console.WriteLine("Enter your guess ");
+
+                    guess = int.Parse(Console.ReadLine());
+
+                    if (guess < targetNumber)
+                        Console.WriteLine("Too low. Try again. ");
+
+                    else if (guess > targetNumber)
+                        Console.WriteLine("Too high. Try again. ");
+                } while (guess != targetNumber);
+
+                Console.WriteLine("You guessed the number!");
+
+                Console.WriteLine("Play Again? (yes/no");
+                playAgain = Console.ReadLine().ToLower();
+
+            } while (playAgain == "yes");
+        }
+    }
+}
+
+
+
+
+
+
