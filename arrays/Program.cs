424276415fdd9e449b70faff235e6682f26a2d2b
ammanh <amman@DESKTOP-VO0EAQH>
2024-09-04 16:35:39 +0530

    alphabeticalOrder

---
+++ arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyApplication
+
+{
+    class Program
+
+    {
+        static void Main()
+
+        {
+
+            string[] names = new string[5];
+
+            Console.WriteLine("Please enter 5 names. ");
+
+            for (int i = 0; i < names.Length; i++)
+
+            {
+                Console.Write($"Name {i + 1}:");
+                names[i] = Console.ReadLine();
+
+            }
+            Array.Sort(names);
+
+            Console.WriteLine("Names in alphabetical order: ");
+
+            for (int i = 0; i < names.Length; i++)
+            {
+
+                Console.WriteLine($"Name {i + 1}: { names[i]}");
+
+            }
+
+        }
+
+            
+    }
+}
--- loopStatements/Program.cs
+++ loopStatements/Program.cs
@@ -16,7 +16,7 @@ namespace myApplication
                 int targetNumber = Random.Next(1, 101);
                 int guess;
 
-                Console.WriteLine("Guess the number i am thinking of between 1 and 101!");
+                Console.WriteLine("Guess the number I am thinking of between 1 and 101!");
 
                 do
                 {
@@ -29,10 +29,10 @@ namespace myApplication
 
                     else if (guess > targetNumber)
                         Console.WriteLine("Too high. Try again. ");
+
                 } while (guess != targetNumber);
 
                 Console.WriteLine("You guessed the number!");
-
                 Console.WriteLine("Play Again? (yes/no");
                 playAgain = Console.ReadLine().ToLower();
 
